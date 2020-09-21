2e54bbbe23f08c5458d490820f575a3b7f94e214
killz4 <asiglesias478@gmail.com>
2020-09-21 19:08:27 +0200

    Reestarted proyect

---
+++ FitnessTrack/FitnessTrack/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using FitnessTrack.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FitnessTrack
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new RoutinePage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
